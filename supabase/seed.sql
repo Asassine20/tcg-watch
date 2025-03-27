03ff227b17de0681bc340ae6ce3db1682ae316d5
Ebsan <6202912+Ebsan@users.noreply.github.com>
2025-03-27 12:44:21 -0400

    Add a new supabase migration to seed the data

---
+++ supabase/seed.sql
@@ -0,0 +1,7 @@
+-- This is a seed file that can be used to populate the price_history table with test data
+-- It will only run if you explicitly call it with supabase db reset --seed
+
+-- You can add any additional seed data here if needed
+-- For example:
+-- INSERT INTO price_history (category_id, group_id, product_id, sub_type_name, prev_date) 
+-- VALUES ('Pokemon', 'Base Set', 12345, 'Holofoil', '2025-03-25');
